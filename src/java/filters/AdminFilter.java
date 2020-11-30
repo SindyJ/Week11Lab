578b1e0ad1bc94fb6d55893fb26e8be3d659a675
726772 <726772@SAIT225751IT.ACDM.DS.SAIT.CA>
2020-11-29 18:37:52 -0700

    initial commit

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import dataaccess.UserDB;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+
+/**
+ *
+ * @author 726772
+ */
+public class AdminFilter implements Filter {
+     
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+            // code that is executed before the servlet
+            HttpServletRequest httpRequest = (HttpServletRequest)request;
+            HttpSession session = httpRequest.getSession();
+            String email = (String)session.getAttribute("email");
+            UserDB userdb = new UserDB();
+            
+            if (email == null) {
+                HttpServletResponse httpResponse = (HttpServletResponse)response;
+                httpResponse.sendRedirect("login");
+                return;
+            }
+            
+            User user = userdb.get(email);
+            if(user.getRole().getRoleId() != 1) {
+                HttpServletResponse httpResponse = (HttpServletResponse)response;
+                httpResponse.sendRedirect("notes");
+                return;
+            }
+            
+            chain.doFilter(request, response); // execute the servlet
+            
+            // code that is executed after the servlet
+    }
+    
+    @Override
+    public void destroy() {        
+    }
+    
+    @Override
+    public void init(FilterConfig filterConfig) {        
+    }
+
+    
+}
